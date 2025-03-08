263738dbec796c4192927823561cee57f5270907
Daysi Schmadtke <daysischmadtke@gmail.com>
2025-03-08 02:16:58 -0500

    issue

--- components/loginButton.js
+++ components/loginButton.js
@@ -2,6 +2,9 @@ import { signIn } from '../utils/auth';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
+  const domString = `<button id="google-login" class="btn btn-info">Login</button>
+`;
+  document.querySelector('#app').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
 
--- public/main.js
+++ public/main.js
@@ -1,37 +1,10 @@
+import 'firebase/compat/auth';
 import 'bootstrap'; // import bootstrap elements and js
-import '../styles/main.scss';
-import { gsap } from 'gsap';
-import { TextPlugin } from 'gsap/TextPlugin';
-import firebase from 'firebase/app';
 import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
-import 'firebase/auth';
-
-gsap.registerPlugin(TextPlugin);
+import '../styles/main.scss';
 
 const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>Logos</h1>
-    <div class='welcome-msg'>
-      <h2> Your <span class="text-info">Digital Board</span> of Ideas, <span class="text-info">Visualize</span> your path to action.</h2>
-    </div>
-    <button id="google-login" class="btn btn-primary">Login with Google</button>
-  `;
   ViewDirectorBasedOnUserAuthStatus();
-
-  // Google login event listener
-  document.querySelector('#google-login').addEventListener('click', () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  });
 };
 
 init();
-
-gsap.to('.welcome-msg', {
-  delay: 5,
-  duration: 7,
-  text: { value: 'Unlock your creativity: Build amazing Projects, develop new ideas!' },
-  color: '#5E2BFF',
-  fontSize: '1.5rem',
-  ease: 'power1.inOut'
-});
