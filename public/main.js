bd327e125e316c68287852068f2192b9863350bc
Daysi Schmadtke <daysischmadtke@gmail.com>
2025-03-08 02:38:41 -0500

    problems logging in

--- public/main.js
+++ public/main.js
@@ -1,19 +1,18 @@
-// USE WITH FIREBASE AUTH
-// import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
+import firebase from 'firebase/compat/app';
+import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
 const init = () => {
   document.querySelector('#app').innerHTML = `
-    <h1>Logos</h1>
-    <h2> Your Digital Board of Ideas, Visualize your path to action.</h2>
-    <button class="btn btn-success" id="login">Login</button><br />
-    <hr />`;
-  // document
-  //   .querySelector('#click-me')
-  //   .addEventListener('click', () => console.warn('You clicked that button!'));
+      <h1>Logos</h1>
+  <div class='welcome-msg'>
+    <h2> Your <span class="text-info">Digital Board</span> of Ideas, <span class="text-info">Visualize</span> your path to action.</h2>
+  </div>
+  <button id="google-auth" class="btn btn-success">GOOGLE LOGIN</button>
+  `;
   // USE WITH FIREBASE AUTH
-  // ViewDirectorBasedOnUserAuthStatus();
+  ViewDirectorBasedOnUserAuthStatus(firebase);
 };
 
 init();
